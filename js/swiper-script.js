094f8dac29a7428062532337952073454a1c83a7
Honorwhite <1onurakbas@gmail.com>
2025-06-13 02:35:00 +0300

    slider

--- js/swiper-script.js
+++ js/swiper-script.js
@@ -213,3 +213,27 @@ $(function () {
         },
     });
 });
+
+$(function () {
+    const videoSwiper = new Swiper('.videoSwiper', {
+        slidesPerView: 1,
+        spaceBetween: 20,
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev'
+        },
+        breakpoints: {
+            1200: {
+                slidesPerView: 3
+            }
+        }
+    });
+});
